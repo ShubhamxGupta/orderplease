fabf9cfc9bfdf74d0822daddb34a75a38ef536ca
Shubham Gupta <shubhamxgupta1@gmail.com>
2025-07-26 15:43:22 +0530

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
     ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        ignores: [
+            "node_modules",
+            ".next",
+            "dist",
+            "build",
+            "public",
+            "app/generated/prisma",
+        ],
+    },
 ];
 
 export default eslintConfig;
